026c3fefa7b48d1081d105993bb9efb4cabc687e
Marcus Rodrigues <rodriguesmarcus@outlook.com>
2020-06-13 15:58:45 -0300

    adição de documentos utilizando webpack 4, react e babel

---
+++ reactjs/my-app2/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Button from "./Button"
+const App = () => {
+    return (
+        <div>
+            <p>Digital Innovation One</p>
+            <p>Trabalho realizado em aula.</p>
+            <p>Utilização do Webpack 4 </p>
+            <div>webpack4</div>
+            <button>teste</button>
+            <Button />
+        </div>
+        // <div><Button /></div>
+    )
+}
+export default App
---
+++ reactjs/my-app2/src/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => (
+    <button>Olá Teste</button>
+);
+
+export default Button;
