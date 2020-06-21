146c85546bea2ce9270c35e568fb01f449f4e69c
Marcus Rodrigues <rodriguesmarcus@outlook.com>
2020-06-21 20:49:10 -0300

    criadção da tabela de eventos para trabalhar com a agenda

--- python_django/agenda_dio/core/admin.py
+++ python_django/agenda_dio/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+
+# para exibir outras informações dentro do nosso BD que por enquanto só exibe o nome
+# do evento
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('titulo','usuario')
+
+admin.site.register(Evento, EventoAdmin)
--- python_django/agenda_dio/core/models.py
+++ python_django/agenda_dio/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Criando uma tabela eventos
+# por padrão os campos não podem ser deixados em branco ou null
 
-# Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    # .CASCADE diz que se o usuário for excluido exclui-se todos os eventos dele
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    # como não criamos primary keys, o django cria um campo id autoincrementável.
+    # a tabela é criada com o nome do app_evento, para forçar que o nome seja só evento:
+    class Meta:
+        db_table  = 'evento'
+
+    # essa função faz com quem o nome do titulo seja invocado quando for chamado o objeto
+    def __str__(self):
+        return self.titulo
