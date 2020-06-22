7717838811fc06f9cd2e638b66e699b4672a0746
Marcus Rodrigues <rodriguesmarcus@outlook.com>
2020-06-21 21:45:12 -0300

    criação de tela de listagem de eventos

--- python_django/agenda_dio/core/models.py
+++ python_django/agenda_dio/core/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 # Criando uma tabela eventos
 # por padrão os campos não podem ser deixados em branco ou null
 
+
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
@@ -12,9 +13,13 @@ class Evento(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     # como não criamos primary keys, o django cria um campo id autoincrementável.
     # a tabela é criada com o nome do app_evento, para forçar que o nome seja só evento:
+
     class Meta:
-        db_table  = 'evento'
+        db_table = 'evento'
 
     # essa função faz com quem o nome do titulo seja invocado quando for chamado o objeto
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M')
--- python_django/agenda_dio/core/views.py
+++ python_django/agenda_dio/core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
+
 
 # Create your views here.
+# Uma maneira de redirecionar a página index para a página desejada:
+# def index(request):
+#     return redirect('/agenda/')
+
+def lista_eventos(request):
+    # Aqui criamos uma filtragem por usuário, vamos manter o all até
+    # começar a trabalhar com autenticação
+    #usuario = request.user
+    #evento = Evento.objects.filter(usuario=usuario)
+    evento = Evento.objects.all()
+    dados = {'eventos': evento}
+    return render(request, 'agenda.html', dados)
